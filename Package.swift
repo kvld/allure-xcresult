083195dd69e763236ecdc470eed9dd2fe380df25
Vladislav Kiryukhin <vkiryukhin@yandex-team.ru>
2022-11-06 23:58:25 +0400

    Add example app

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "AllureXCResultLib", targets: ["AllureXCResultLib"])
     ],
     dependencies: [
-        .package(url: "https://github.com/kvld/XCResultKit.git", .revisionItem("97d1d4b")),
+        .package(url: "https://github.com/davidahouse/XCResultKit.git", from: "1.0.1"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.2"),
     ],
     targets: [
